d16871c65bd4fdcbb7d49497659da719de446a3a
manoj.k <manoj.k@sonata-software.com>
2018-02-12 18:11:47 +0530

    spawning food

--- src/SolidSnake.java
+++ src/SolidSnake.java
@@ -13,9 +13,55 @@ public class SolidSnake extends JPanel {
     static int DIRECTION = 0;
     Random rand = new Random();
     ArrayList<Pair> body = new ArrayList<Pair>();
+    Pair food = new Pair(50, 50);
+    boolean gameover =false;
 
     private void moveBall() {
 
+    }
+
+    public int randomWithRange(int max, int min) {
+        int range = (max - min) + 1;
+        return (int) Math.random() * range + min;
+    }
+
+    public void setValidRange() {
+        while (true) {
+            int x = randomWithRange(0, 38) * 10;
+            int y = randomWithRange(0, 38) * 10;
+
+            Pair p = new Pair(x, y);
+            if (!body.contains(p)) {
+                food.setFi(p.getFi());
+                food.setSe(p.getSe());
+                break;
+            }
+        }
+
+    }
+
+    public boolean detectCollision(int topx, int topy) {
+        if (topx == food.getFi() && topy == food.getSe()) {
+            setValidRange();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void checkGameOver(int topx,int topy){
+        Pair p = new Pair(topx,topy);
+            if(body.contains(p))
+                gameover = true;
+            else
+               gameover = false;
+
+    }
+
+
+    private void spwanFood(Pair food, Graphics2D g2d) {
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(food.getFi() + 1, food.getSe(), 9, 9);
     }
 
 
@@ -36,8 +82,8 @@ public class SolidSnake extends JPanel {
     public int modulus(int x) {
         if (x >= 400)
             x = 0;
-        if(x < 0)
-            x=390;
+        if (x < 0)
+            x = 390;
         return x;
 
     }
@@ -45,7 +91,7 @@ public class SolidSnake extends JPanel {
     public void traversal() {
         if (body.size() > 0) {
 
-            for (int i = body.size() - 2; i >= 0; i--) {
+            for (int i = 0; i < body.size() - 1; i++) {
                 body.get(i).setFi(body.get(i + 1).getFi());
                 body.get(i).setSe(body.get(i + 1).getSe());
             }
@@ -53,16 +99,32 @@ public class SolidSnake extends JPanel {
             int topy = body.get(body.size() - 1).getSe();
             switch (DIRECTION) {
                 case 0:
-                    body.get(body.size() - 1).setFi(modulus(topx + 10));
+                    checkGameOver(topx + 10, topy);
+                    if (detectCollision(topx + 10, topy))
+                        body.add(new Pair(topx + 10, topy));
+                    else
+                        body.get(body.size() - 1).setFi(modulus(topx + 10));
                     break;
                 case 1:
-                    body.get(body.size() - 1).setFi(modulus(topx - 10));
+                    checkGameOver(topx - 10, topy);
+                    if (detectCollision(topx - 10, topy))
+                        body.add(new Pair(topx - 10, topy));
+                    else
+                        body.get(body.size() - 1).setFi(modulus(topx - 10));
                     break;
                 case 2:
-                    body.get(body.size() - 1).setSe(modulus(topy + 10));
+                    checkGameOver(topx , topy+10);
+                    if (detectCollision(topx, topy + 10))
+                        body.add(new Pair(topx, topy + 10));
+                    else
+                        body.get(body.size() - 1).setSe(modulus(topy + 10));
                     break;
                 case 3:
-                    body.get(body.size() - 1).setSe(modulus(topy - 10));
+                    checkGameOver(topx , topy-10);
+                    if (detectCollision(topx, topy - 10))
+                        body.add(new Pair(topx, topy - 10));
+                    else
+                        body.get(body.size() - 1).setSe(modulus(topy - 10));
                     break;
             }
 
@@ -76,6 +138,7 @@ public class SolidSnake extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         printSnake(body, g2d);
+        spwanFood(food, g2d);
        /* g2d.setColor(Color.BLACK);
         g2d.fillRect(x, y, 10, 10);*/
 
@@ -125,6 +188,8 @@ public class SolidSnake extends JPanel {
 
 
         while (true) {
+            if(snake.gameover)
+                break;
             snake.traversal();
             snake.repaint();
 
@@ -163,6 +228,24 @@ class Pair {
 
     public void setSe(Integer se) {
         this.se = se;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (!fi.equals(pair.fi)) return false;
+        return se.equals(pair.se);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fi.hashCode();
+        result = 31 * result + se.hashCode();
+        return result;
     }
 }
 
